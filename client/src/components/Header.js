d9e08c96e03b937ae546bd4c5cb5a956d5d903c8
Paola Garay <garay.angela@gmail.com>
2020-10-06 00:29:48 +0200

    Redux setup and Authentication with Google

---
+++ client/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import GooglerAuth from './GooglerAuth';
+
+export default function Header() {
+    return (
+        <div className="ui secondary pointing menu">
+            <Link to="/" className="item">Streamy</Link>
+            <div className="right menu">
+            <Link to="/" className="item">All Streams</Link>
+            <GooglerAuth />
+            </div>
+        </div>
+    )
+}
